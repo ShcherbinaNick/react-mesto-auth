a1886a586edcbf77d863412c0d67618da3b7d857
ShcherbinaNick <scherbinanick@yandex.ru>
2022-02-02 13:36:12 +0300

    second review

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,8 +1,16 @@
 function PopupWithForm({ name, title, buttonText="Сохранить", children, isOpen, onClose, onSubmit }) {
   
+  function handleClickOverlay(e) {
+    if (isOpen) {
+      if(e.target.classList.contains('popup')) {
+       onClose()
+    }
+    }
+  }
+  
   return (
     <>
-      <article className={`popup popup_type_${ name } ${isOpen ? "popup_active" : "" }` } onClick={ onClose }>
+      <article className={`popup popup_type_${ name } ${isOpen ? "popup_active" : "" }` } onClick={ handleClickOverlay }>
         <form onSubmit={ onSubmit } className="popup__container" name={` ${ name }-form`}>
           <button type="button" aria-label="закрыть" className="popup__close-button" onClick={ onClose }></button>
           <h2 className="popup__title">{ title }</h2>
